3a558123c4154e75e92ed628a868b51335399f67
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2024-05-19 17:31:25 +0530

    updated deployment

--- config/database.js
+++ config/database.js
@@ -1,30 +1,17 @@
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
-
-  const connections = {
-    postgres: {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: env("DATABASE_HOST", "127.0.0.1"),
-          port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "strapi"),
-          user: env("DATABASE_USERNAME", ""),
-          password: env("DATABASE_PASSWORD", ""),
-          ssl: {
-            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-          },
-        },
-        debug: true,
+module.exports = ({ env }) => ({
+  connection: {
+    client: "pg",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "toor"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-    },
-  };
-};
+    debug: false,
+  },
+});
