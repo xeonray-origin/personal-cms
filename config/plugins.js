341db55e701256e9f06d4555857e8af14b774544
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2024-05-29 10:53:04 +0530

    changed to signed

--- config/plugins.js
+++ config/plugins.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
           baseUrl: `https://s3.us-east-1.amazonaws.com/personal-cms-content`, // This line sets the custom url format
           region: env("AWS_REGION"),
           params: {
-            ACL: env("AWS_ACL", "public-read"),
+            ACL: "private",
             signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
             Bucket: env("AWS_BUCKET_NAME"),
           },
